f806502b5baf8bea55c4df799dab2ab2a8401b4e
TuTuanLe <74090975+TuTuanLe@users.noreply.github.com>
2022-05-09 19:44:51 +0700

    Initialize feature/story

---
+++ app/src/main/java/com/tutuanle/chatapp/activities/ChatActivity.java
@@ -0,0 +1,137 @@
+package com.tutuanle.chatapp.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tutuanle.chatapp.adapters.MessagesAdapter;
+import com.tutuanle.chatapp.databinding.ActivityChatBinding;
+import com.tutuanle.chatapp.models.Message;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+public class ChatActivity extends AppCompatActivity {
+
+    ActivityChatBinding binding;
+    MessagesAdapter adapter;
+    ArrayList<Message> messages;
+
+    String senderRoom, receiveRoom;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        String name = getIntent().getStringExtra("name");
+        String receiverUid = getIntent().getStringExtra("uid");
+        String senderUid = FirebaseAuth.getInstance().getUid();
+
+        senderRoom = senderUid + receiverUid;
+        receiveRoom = receiverUid + senderUid;
+
+        messages = new ArrayList<>();
+        adapter = new MessagesAdapter(this, messages, senderRoom, receiveRoom);
+        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        database = FirebaseDatabase.getInstance();
+
+        database.getReference().child("Chats")
+                .child(senderRoom)
+                .child("messages")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messages.clear();
+                        for(DataSnapshot snapshot1: snapshot.getChildren()){
+                            Message message = snapshot1.getValue(Message.class);
+                            message.setMessageId(snapshot1.getKey());
+                            messages.add(message);
+                            Log.d("onDataChange: ", message.toString());
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String messageTxt = binding.messageBox.getText().toString();
+
+                Date date = new Date();
+
+                Message message = new Message(messageTxt, senderUid, date.getTime());
+
+
+
+                binding.messageBox.setText("");
+
+                String randomKey = database.getReference().push().getKey();
+
+                HashMap<String, Object> lastMsgObj =  new HashMap<>();
+                lastMsgObj.put("lastMsg", message.getMessage());
+                lastMsgObj.put("lastMsgTime", message.getTimestamp());
+
+                database.getReference().child("Chats").child(senderRoom).updateChildren(lastMsgObj);
+                database.getReference().child("Chats").child(receiveRoom).updateChildren(lastMsgObj);
+
+                database.getReference().child("Chats")
+                        .child(senderRoom)
+                        .child("messages")
+                        .child(randomKey)
+                        .setValue(message)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                database.getReference().child("Chats")
+                                        .child(receiveRoom)
+                                        .child("messages")
+                                        .child(randomKey)
+                                        .setValue(message)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+
+                                            }
+                                        });
+
+
+                            }
+                        });
+
+
+            }
+        });
+
+
+        getSupportActionBar().setTitle(name);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
